1bbb1d794ab267317b5e3e9bfe63dea508343cfc
daviresende03 <daviresendedev@gmail.com>
2022-12-05 19:12:55 -0300

    UnitOfWork parte 1

---
+++ Order.Domain/Interfaces/Repositories/DataConnector/IDbConnector.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Order.Domain.Interfaces.Repositories.DataConnector
+{
+    public interface IDbConnector : IDisposable
+    {
+        IDbConnection dbConnection { get; set; }
+        IDbTransaction dbTransaction { get; set; }
+    }
+}
---
+++ Order.Infra/DataConnector/MySqlConnector.cs
@@ -0,0 +1,16 @@
+﻿using Order.Domain.Interfaces.Repositories.DataConnector;
+using System.Data;
+
+namespace Order.Infra.DataConnector
+{
+    internal class MySqlConnector : IDbConnector
+    {
+        public IDbConnection dbConnection { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IDbTransaction dbTransaction { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
